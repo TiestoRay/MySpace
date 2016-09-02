66a04c533de2478da1d1c17c537893dd7788d146
tiestoray <276422918@qq.com>
2016-09-02 10:13:47 +0800

    tt

--- WebApiServer/WebApiServer/Global.asax.cs
+++ WebApiServer/WebApiServer/Global.asax.cs
@@ -20,8 +20,6 @@ namespace WebApiServer
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
